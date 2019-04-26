984a34f3ad82927f11527b5f9c3f973e1db15730
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2019-04-25 23:05:12 -0700

    csh_test_envs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="netenv",
     version="0.1",
     packages=["netenv"],
-    install_requires=["gym~=0.10", "numpy~=1.14"],
+    install_requires=["gym~=0.10.0", "numpy~=1.14"],
     extras_require={"dev": ["pytest", "pytest-benchmark"]},
     use_scm_version=False
     if os.environ.get("USE_SCM_VERSION", "1") == "0"
